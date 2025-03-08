57ebc7e7ea4a2f810eb518e0606da5c3bd1fd940
Mohfazam <mohd.sarwar.code@gmail.com>
2025-03-08 14:21:00 +0530

    Power Of Two without Loops

--- Number System/Problems/Powerof2.cpp
+++ Number System/Problems/Powerof2.cpp
@@ -2,19 +2,26 @@
 
 #include<iostream>
 
+// bool isPowerOfTwo(int n){
+//     if(n <= 0){
+//         return false;
+//     }
+
+//     while(n > 1){
+//         if(n % 2 != 0){
+//             return false;
+//         }
+//         n = n/2;
+//     }
+
+//     return true;
+// }
+
 bool isPowerOfTwo(int n){
-    if(n <= 0){
-        return false;
+    if((n & (n-1)) == 0){
+        return true;
     }
-
-    while(n > 1){
-        if(n % 2 != 0){
-            return false;
-        }
-        n = n/2;
-    }
-
-    return true;
+    return false;
 }
 using namespace std;
 
