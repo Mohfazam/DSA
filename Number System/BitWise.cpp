b22665c51cd7128ff05b3caf180cccda89ff4aa0
Mohfazam <mohd.sarwar.code@gmail.com>
2025-03-08 13:26:05 +0530

    AND, OR, XOR, >>, <<

---
+++ Number System/BitWise.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"6 AND 10: " <<(6 & 10) <<endl;
+    cout<<"6 OR 10: " << (6 | 10) <<endl;
+    cout<<"6 XOR 10: " << (6 ^ 10) <<endl;
+    cout<<"10 << 2: " << (10<<2) <<endl;
+    cout<<"10 >> 1: " << (10>>1) <<endl;
+    cout<<"7 >> 1: " << (7>>1) <<endl;
+
+    return 0;
+}
