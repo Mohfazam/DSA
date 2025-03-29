66a001a212053f8f7044c6bd732d31f8710dc02b
Mohfazam <mohd.sarwar.code@gmail.com>
2025-03-30 03:49:11 +0530

    Smallest And Largest in the array

--- Arrays/3_SmallestAndLargest.cpp
+++ Arrays/3_SmallestAndLargest.cpp
@@ -1,23 +1,25 @@
 //smallest and largest number in an array
 #include<iostream>
 #include<climits>
+#include<algorithm>
 
 using namespace std;
 
 int main(){
     int min = INT_MAX;
-    int max = INT_MIN;
+    int maxx = INT_MIN;
     int size = 10;
     int arr[size];
     for(int i = 0; i < size; i++){
-        cout<<"Enter the element at the index: "<<i<<" ";
+        cout<<"Enter the element at the index: "<<i<<" : ";
         cin>>arr[i];
     }
 
     //finding smallest and largest
     for(int i = 0; i < size; i++){
-        if(arr[i] > max){
-            max = arr[i];
+        if(arr[i] > maxx){
+            // max = arr[i];
+            maxx = max(arr[i], maxx);
         }
 
         if(arr[i] < min){
@@ -25,5 +27,5 @@ int main(){
         }
     }
 
-    cout<<"The Smallest number is : "<<min<<endl<<"The Largest Number is : "<<max<<endl;
+    cout<<"The Smallest number is : "<<min<<endl<<"The Largest Number is : "<<maxx<<endl;
 }
