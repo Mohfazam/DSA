52c4038124b40502de5f3414095cb64b8be3b1f6
Mohfazam <mohd.sarwar.code@gmail.com>
2025-05-07 21:30:39 +0530

    Vector Functions2

--- Vectors/Second/Second.cpp
+++ Vectors/Second/Second.cpp
@@ -7,7 +7,16 @@ int main(){
 
     vector<int> vec = {1, 2, 3, 4, 5};
 
-    cout<<vec.size();
+    cout<<vec.size()<<endl;
+
+    for(int i : vec){
+        cout<<i<<endl;
+    }
+    vec.push_back(6);
+    cout<<"Added Element to the back"<<endl;
+    for(int i : vec){
+        cout<<i<<endl;
+    }
 
 
     return 0;
