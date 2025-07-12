ab5f378f99fa71d7c62fb36ff29f5ac5e6a12d32
Mohfazam <mohd.sarwar.code@gmail.com>
2025-07-13 04:52:28 +0530

    Pointers

--- Pointers/passbyreferance.cpp
+++ Pointers/passbyreferance.cpp
@@ -2,16 +2,16 @@
 
 using namespace std;
 
-int changeA(int *a){
-    *a = *a + 10;
-    return *a;
+int changeA(int &x){
+    x = x + 10;
+    return x;
 }
 
 int main(){
 
     int a = 10;
     cout<<a<<endl;
-    cout<<changeA(&a)<<endl;
+    cout<<changeA(a)<<endl;
     cout<<a<<endl;
 
 
