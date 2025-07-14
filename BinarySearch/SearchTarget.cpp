bb55efe01cefb1e67c619b234bbc597ea3f86908
Mohfazam <mohd.sarwar.code@gmail.com>
2025-07-14 23:21:15 +0530

    binary Search

--- BinarySearch/SearchTarget.cpp
+++ BinarySearch/SearchTarget.cpp
@@ -5,12 +5,25 @@ using namespace std;
 int main(){
     int target;
     int n;
+    int arr[n];
 
-    cout<<"Enter the Size of the array";
+    cout<<"Enter the Size of the array: ";
     cin>>n;
 
-    cout<<"Enter the Target Value";
+    cout<<"Enter the Target Value: ";
     cin>>target;
 
+    for(int i = 0; i < n; i++){
+        cout<<"Enter the Value of index "<<i+1<<": ";
+        cin>>arr[i];
+    }
+
+    cout<<"Array elements "<<endl;
+    for(int i = 0; i < n; i++){
+        cout<<"Value: "<<i<<arr[i]<<endl;
+        
+    }
+
+    return 0;
 
 }
