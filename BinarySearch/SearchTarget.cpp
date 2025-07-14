0c6979bba3e5e5fea1da0bfb7f7fdc176732bf45
Mohfazam <mohd.sarwar.code@gmail.com>
2025-07-14 23:16:25 +0530

    binary Search

---
+++ BinarySearch/SearchTarget.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int target;
+    int n;
+
+    cout<<"Enter the Size of the array";
+    cin>>n;
+
+    cout<<"Enter the Target Value";
+    cin>>target;
+
+
+}
