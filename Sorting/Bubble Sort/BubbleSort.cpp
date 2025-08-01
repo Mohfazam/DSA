3cbd9d3023e8462f54cf4992020d59eea29a4b4a
Mohfazam <mohd.sarwar.code@gmail.com>
2025-08-01 23:10:56 +0530

    Bubble Sort

--- Sorting/Bubble Sort/BubbleSort.cpp
+++ Sorting/Bubble Sort/BubbleSort.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 
 int main(){
-    vector<int> nums = {15, 16, 18, 55};
+    vector<int> nums = {85, 16, 18, 55};
 
     int n = nums.size();
 
     for(int i = 0; i < n-1; i++){
         for(int j = 0; j < n-i-1; j++){
-            if(nums[j] < nums[j+1]){
+            if(nums[j] > nums[j+1]){
                 swap(nums[j], nums[j+1]);
             }
         }
