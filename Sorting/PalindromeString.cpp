08819ab77282c939a54908c94a0c737d1ee7c973
Mohfazam <mohd.sarwar.code@gmail.com>
2025-11-29 00:40:14 +0530

    palindrome

---
+++ Sorting/PalindromeString.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+bool isPalindrome(string str, int s, int e){
+
+    if(s >= e){
+        return true;
+    }
+    
+
+    if(str[s] != str[e]){
+        return false;
+    }
+
+    return isPalindrome(str, s+1, e-1);
+}
+
+
+
+int main(){
+
+    string str = "amsdma";
+
+
+    cout<<isPalindrome(str, 0, str.size()-1);
+
+}
