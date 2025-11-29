11700f28815f440cca7144e4e145b560f206c805
Mohfazam <mohd.sarwar.code@gmail.com>
2025-11-29 15:09:05 +0530

    Character hash

---
+++ Hashing/characterHashtable.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int charHash[26] = {0};
+
+    string ch;
+    cout<<"Enter the String: ";
+    cin>>ch;
+
+    for(int i =0; i < ch.size(); i++){
+        charHash[ch[i] - 'a']++;
+    }
+
+    for(int i = 0; i < 26; i++){
+        cout<<char(i+'a')<<" -> ";
+        cout<<charHash[i]<<endl;
+    }
+    return 0;
+}
