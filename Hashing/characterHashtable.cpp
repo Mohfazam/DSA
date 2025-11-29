2e3a06658f5a2f40bad0b328823066f32a48230c
Mohfazam <mohd.sarwar.code@gmail.com>
2025-11-29 15:15:37 +0530

    Character hash

--- Hashing/characterHashtable.cpp
+++ Hashing/characterHashtable.cpp
@@ -1,20 +1,48 @@
+// #include<bits/stdc++.h>
+
+// using namespace std;
+
+// int main(){
+//     int charHash[26] = {0};
+
+//     string ch;
+//     cout<<"Enter the String: ";
+//     cin>>ch;
+
+//     for(int i =0; i < ch.size(); i++){
+//         charHash[ch[i] - 'a']++;
+//     }
+
+//     for(int i = 0; i < 26; i++){
+//         cout<<char(i+'a')<<" -> ";
+//         cout<<charHash[i]<<endl;
+//     }
+//     return 0;
+// }
+
+
+
+
+
+
 #include<bits/stdc++.h>
 
 using namespace std;
 
 int main(){
-    int charHash[26] = {0};
+    int charHash[256] = {0};
 
     string ch;
     cout<<"Enter the String: ";
     cin>>ch;
 
     for(int i =0; i < ch.size(); i++){
-        charHash[ch[i] - 'a']++;
+        charHash[ch[i]]++;
     }
 
-    for(int i = 0; i < 26; i++){
-        cout<<char(i+'a')<<" -> ";
+
+    for(int i = 0; i < 256; i++){
+        cout<<char(i)<<" -> ";
         cout<<charHash[i]<<endl;
     }
     return 0;
