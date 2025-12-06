3956c77b8f35cc6028898a96cdde2dbb08abdcb7
Mohfazam <mohd.sarwar.code@gmail.com>
2025-12-06 21:40:30 +0530

    2Sum

---
+++ Arrays/2Sum.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+vector<int> twoSumbWithoutMap(vector<int>& nums, int target) {
+        vector<int> temp = (nums);
+        sort(nums.begin(), nums.end());
+        int a = 0, b = 0;
+        int i = 0;
+        int j = nums.size()-1;
+
+        while(i < j){
+            if(nums[i] + nums[j] == target){
+                a = nums[i];
+                b = nums[j];
+                break;
+            }
+            else if(nums[i] + nums[j] > target){
+                j--;
+            }else{
+                i++;
+            }
+        }
+
+        int x  = -1;
+        int y = -1;
+
+        for(int i = 0; i < temp.size(); i++){
+            if(temp[i] == a && x == -1){
+                x = i;
+            }else if(temp[i] == b && y == -1){
+                y = i;
+            }
+        }
+
+        return {x, y};
+    }
+
+
+//Better Solution
+vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> ans;
+
+        for(int i = 0; i < nums.size(); i++){
+            int a = nums[i];
+            int more = target - a;
+
+            if(ans.find(more) != ans.end()){
+                return {ans[more], i};
+            }
+
+            ans[a] = i;
+        }
+
+        return {};
+    }
+
+int main(){
+    vector<int> nums = {2,7,11,15}; 
+    int target = 9;
+    vector<int> ans = twoSumbWithoutMap(nums, target);
+    cout << "[" << ans[0] << ", " << ans[1] << "]";
+    return 0;
+}
