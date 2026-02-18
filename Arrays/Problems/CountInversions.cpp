1b6997149ff3e9cfd09ce86082d5a60a44eb54cb
Mohfazam <mohd.sarwar.code@gmail.com>
2026-02-19 02:50:07 +0530

    COuntInversions Optimal

--- Arrays/Problems/CountInversions.cpp
+++ Arrays/Problems/CountInversions.cpp
@@ -1,22 +1,59 @@
 #include<bits/stdc++.h>
-
 using namespace std;
+
+int mergeArray(int arr[], int low, int mid, int high){
+    int left = low;
+    int right = mid + 1;
+    int index = 0;
+
+    int temp[high - low + 1];
+    int count = 0;
+
+    while(left <= mid && right <= high){
+        if(arr[left] <= arr[right]){
+            temp[index++] = arr[left++];
+        } else {
+            count += (mid - left + 1);
+            temp[index++] = arr[right++];
+        }
+    }
+
+    while(left <= mid){
+        temp[index++] = arr[left++];
+    }
+
+    while(right <= high){
+        temp[index++] = arr[right++];
+    }
+
+    for(int i = low; i <= high; i++){
+        arr[i] = temp[i - low];
+    }
+
+    return count;
+}
+
+int mergeSort(int arr[], int low, int high){
+    if(low >= high){
+        return 0;
+    }
+
+    int mid = low + (high - low) / 2;
+
+    int left = mergeSort(arr, low, mid);
+    int right = mergeSort(arr, mid + 1, high);
+    int merge = mergeArray(arr, low, mid, high);
+
+    return left + right + merge;
+}
 
 int main(){
     int arr[] = {1,3,5,10,2,6,8,9};
     int n = 8;
 
-    int count = 0;
+    int final = mergeSort(arr, 0, n-1);
 
-    for(int i = 0; i < n; i++){
-        for(int j = i+1; j < n; j++){
-            if(i < j && arr[i] > arr[j]){
-                count++;
-            }
-        }
-    }
-
-    cout<<"The count of inversions is: "<<count;
+    cout<<"The count of inversions is: "<<final;
 
     return 0;
 }
