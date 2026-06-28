4ccf52f5f0276d5d10d0c8a40756ab107c6c8ccd
Mohfazam <mohd.sarwar.code@gmail.com>
2026-06-28 16:10:40 +0530

    LL Traversal

---
+++ Linked List/TraverseLL.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node *next;
+
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+
+
+int main(){
+    vector<int> nums = {2, 5, 10, 15};
+
+    Node *head = new Node(nums[0]);
+    Node *tail = head;
+
+    for(int i = 1; i < nums.size(); i++){
+        Node *temp = new Node(nums[i]);
+        tail->next = temp;
+        tail = temp;
+
+        
+        
+    }
+
+    Node *temp = head;
+
+    while(temp){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+
+    return 0;
+}
